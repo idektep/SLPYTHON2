81590dd79e487b117fe4988c46b3b863b98f767c
idektep <idektep.coach@gmail.com>
2026-04-21 21:51:43 +0700

    Add files via upload

---
+++ Workshop/w00_subscribe_all.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+BROKER = "broker.emqx.io"
+TOPIC = "idt/demo/#"
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected:", rc)
+    client.subscribe(TOPIC)
+    print("Subscribed to:", TOPIC)
+
+def on_message(client, userdata, msg):
+    print(f"[{msg.topic}] {msg.payload.decode()}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(BROKER, 1883, 60)
+client.loop_forever()
