b180031a22eed2f209a90c3d96ec44158ae89824
idektep <idektep.coach@gmail.com>
2026-04-21 19:56:47 +0700

    Add files via upload

---
+++ Workshop/w00_publisher_auto.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import json
+import time
+import random
+
+BROKER = "broker.emqx.io"
+TOPIC = "idt/sensor/demo"
+
+client = mqtt.Client()
+client.connect(BROKER, 1883, 60)
+
+print("Publisher started...")
+print("Sending data to topic:", TOPIC)
+
+while True:
+    data = {
+        "temp": round(random.uniform(25, 35), 2),
+        "hum": random.randint(40, 80)
+    }
+
+    payload = json.dumps(data)
+    client.publish(TOPIC, payload)
+
+    print("Send:", payload)
+    time.sleep(2)
